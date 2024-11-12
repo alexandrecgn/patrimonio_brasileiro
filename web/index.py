ef9af369a01f4e7cda227bb4567b9253988817ed
Alexandre Cavalcanti <alexandrecgn@proton.me>
2024-11-12 18:53:37 -0300

    Ajustar layout dos resultados web

--- web/index.py
+++ web/index.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import geopandas as gpd
+from streamlit_folium import st_folium
 from busca_web import pesquisar, refinar_material, refinar_imaterial
 
 
@@ -28,8 +30,8 @@ area = st.file_uploader("Selecionar área", type=["kml", "gpkg", "geojson"])
 enviado = st.button("Pesquisar")
     
 if enviado:
-    with st.status("## Pesquisando Bens Culturais na área inserida", expanded=True) as status:
-        sitios = pesquisar(area, base_sitios)
+    with st.status("Pesquisando Bens Culturais na área inserida", expanded=True) as status:
+        sitios= pesquisar(area, base_sitios)
         imaterial = pesquisar(area, base_imaterial)
         tombados = pesquisar(area, base_tombados)
         valorados = pesquisar(area, base_valorados)
@@ -37,20 +39,33 @@ if enviado:
         tab_imt = refinar_imaterial(imaterial)
         tab_tmb = refinar_material(tombados, "Patrimônio Tombado")
         tab_val = refinar_material(valorados, "Patrimônio Ferroviário")
-        status.update(label="## Pesquisa Concluída", state="complete")
+        status.update(label="Pesquisa Concluída", state="complete")
 
+    tab1, tab2, tab3, tab4 = st.tabs(["Patrimônio Arqueológico", "Patrimônio Imaterial", "Patrimônio Tombado", "Patrimônio Ferroviário"])
+
+    with tab1:
+        st.header("Sítios Arqueológicos Cadastrados")
         if tab_sit.empty:
             st.write("Não foi identificado Patrimônio Arqueológico na área de busca")
         if not tab_sit.empty:
-            st.dataframe(tab_sit)
+            st.dataframe(tab_sit
+    
+    with tab2:
+        st.header("Bens Imateriais Registrados")
         if tab_imt.empty:
             st.write("Não foi identificado Patrimônio Imaterial na área de busca")
         if not tab_imt.empty:
             st.dataframe(tab_imt)
+    
+    with tab3:
+        st.header("Bens Materiais Tombados")
         if tab_tmb.empty:
             st.write("Não foi identificado Patrimônio Tombado na área de busca")
         if not tab_tmb.empty:
             st.dataframe(tab_tmb)
+    
+    with tab4:
+        st.header("Bens Materiais Valorados")
         if tab_val.empty:
             st.write("Não foi identificado Patrimônio Ferroviário na área de busca")
         if not tab_val.empty:
