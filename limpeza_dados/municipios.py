6cab51cafd77ca25fe42f4f5f4b68ed092f5ecc5
Alexandre Cavalcanti <alexandrecgn@proton.me>
2025-01-09 23:07:35 -0300

    Ajustar script de separação dos polígonos dos municípios

--- limpeza_dados/municipios.py
+++ limpeza_dados/municipios.py
@@ -3,8 +3,8 @@ import json
 with open("geo/municipios.geojson") as arquivo:
     municipios = json.load(arquivo)
     print("Arquivo de municípios carregado com sucesso!")
+    i = 0
     for item in municipios["features"]:
-        i = 0
         with open(f"municipios/{item["properties"]["nm_mun"]}.geojson", "a") as cidade:
             json.dump(item, cidade, indent=4)
         print(f"Município {i} salvo com sucesso!")
