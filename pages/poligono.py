46fd98701dbd10ba8d377945af1fcb33d2ffe750
Alexandre Cavalcanti <alexandrecgn@proton.me>
2025-01-17 22:42:59 -0300

    Adicionar cores aos marcadores

--- pages/poligono.py
+++ pages/poligono.py
@@ -7,6 +7,11 @@ from utils import pesquisar, to_dict, refinar_material, refinar_imaterial
 
 mapinha = folium.Map(tiles="Esri WorldImagery", control_scale=True)
 
+# sitios - cinza
+# imaterial - roxo
+# tombados - verde
+# valorados - azul
+
 base_sitios = "http://portal.iphan.gov.br/geoserver/SICG/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=SICG%3Asitios&maxFeatures=2147483647&outputFormat=application%2Fjson"
 base_imaterial_pol = "https://raw.githubusercontent.com/alexandrecgn/buscador_patrimonio/refs/heads/main/bens/imaterial_pol.geojson"
 base_imaterial_pt = "https://raw.githubusercontent.com/alexandrecgn/buscador_patrimonio/refs/heads/main/bens/imaterial_pt.geojson"
@@ -27,8 +32,16 @@ with st.form("busca"):
     enviado = st.form_submit_button("Pesquisar")
     
     if enviado:
-        folium.GeoJson(gpd.read_file(area), name="Polígono de busca").add_to(mapinha)
-        with st.status("Pesquisando Bens Culturais na área inserida", expanded=True) as status:
+        folium.GeoJson(
+            gpd.read_file(area),
+            name="Polígono de busca",
+            style_function=lambda cor: {"color": "red"},
+            ).add_to(mapinha)
+
+        with st.status(
+            "Pesquisando Bens Culturais na área inserida",
+            expanded=True,
+            ) as status:
             sitios = pesquisar(area, base_sitios)
             imaterial_pol = pesquisar(area, base_imaterial_pol)
             imaterial_pt = pesquisar(area, base_imaterial_pt)
@@ -55,46 +68,91 @@ with st.form("busca"):
             
             status.update(label="Pesquisa Concluída", state="complete")
 
-        tab1, tab2, tab3, tab4 = st.tabs(["Patrimônio Arqueológico", "Patrimônio Imaterial", "Patrimônio Tombado", "Patrimônio Ferroviário"])
+        tab1, tab2, tab3, tab4 = st.tabs([
+            "Patrimônio Arqueológico",
+            "Patrimônio Imaterial",
+            "Patrimônio Tombado",
+            "Patrimônio Ferroviário",
+            ])
 
         with tab1:
             st.header("Sítios Arqueológicos Cadastrados")
+            
+            icon = folium.Icon(color="lightgray")
+
             if tab_sit.empty:
                 st.write("Não foi identificado Patrimônio Arqueológico na área de busca")
             if not tab_sit.empty:
                 st.dataframe(tab_sit, use_container_width=True)
-                folium.GeoJson(sitios["geometry"], name="Bens Arqueológicos").add_to(mapinha)
+                folium.GeoJson(
+                    sitios["geometry"],
+                    name="Bens Arqueológicos",
+                    marker=folium.Marker(icon=icon),
+                    ).add_to(mapinha)
         
         with tab2:
             st.header("Bens Imateriais Registrados")
+            
+            icon = folium.Icon(color="purple")
+
             if tab_imtpol.empty and tab_imtpt.empty:
                 st.write("Não foi identificado Patrimônio Imaterial na área de busca")
             elif tab_imtpol.empty and not tab_imtpt.empty:
                 st.dataframe(tab_imtpt, use_container_width=True)
-                folium.GeoJson(imaterial_pol["geometry"]).add_to(mapinha)
+                folium.GeoJson(
+                    imaterial_pt["geometry"],
+                    name="Bens Registrados (pontos)",
+                    marker=folium.Marker(icon=icon),
+                    ).add_to(mapinha)
             elif tab_imtpt.empty and not tab_imtpol.empty:
                 st.dataframe(tab_imtpol, use_container_width=True)
-                folium.GeoJson(imaterial_pt["geometry"]).add_to(mapinha)
+                folium.GeoJson(
+                    imaterial_pol["geometry"],
+                    name="Bens Registrados (polígonos)",
+                    style_function=lambda cor: {"color": "purple"},
+                    ).add_to(mapinha)
             elif not tab_imtpt.empty and not tab_imtpol.empty:
                 st.dataframe(tab_im_tot, use_container_width=True)
-                folium.GeoJson(imaterial_pol["geometry"], name="Bens Registrados (polígonos)").add_to(mapinha)
-                folium.GeoJson(imaterial_pt["geometry"], name="Bens Registrados (pontos)").add_to(mapinha)
+                folium.GeoJson(
+                    imaterial_pol["geometry"],
+                    name="Bens Registrados (polígonos)",
+                    style_function=lambda cor: {"color": "purple"},
+                    ).add_to(mapinha)
+                folium.GeoJson(
+                    imaterial_pt["geometry"],
+                    name="Bens Registrados (pontos)",
+                    marker=folium.Marker(icon=icon),
+                    ).add_to(mapinha)
         
         with tab3:
             st.header("Bens Materiais Tombados")
+
+            icon = folium.Icon(color="green")
+
             if tab_tmb.empty:
                 st.write("Não foi identificado Patrimônio Tombado na área de busca")
             if not tab_tmb.empty:
                 st.dataframe(tab_tmb, use_container_width=True)
-                folium.GeoJson(tombados["geometry"], name="Bens Tombados").add_to(mapinha)
+                folium.GeoJson(
+                    tombados["geometry"],
+                    name="Bens Tombados",
+                    marker=folium.Marker(icon=icon),
+                    ).add_to(mapinha)
         
         with tab4:
             st.header("Bens Materiais Valorados")
+
+            icon = folium.Icon(color="blue")
+
             if tab_val.empty:
                 st.write("Não foi identificado Patrimônio Ferroviário na área de busca")
             if not tab_val.empty:
                 st.dataframe(tab_val, use_container_width=True)
-                folium.GeoJson(valorados["geometry"], name="Bens Valorados").add_to(mapinha)
+                folium.GeoJson(
+                    valorados["geometry"],
+                    name="Bens Valorados",
+                    marker=folium.Marker(icon=icon),
+                    ).add_to(mapinha)
 
         folium.LayerControl().add_to(mapinha)
         st_folium(mapinha)
