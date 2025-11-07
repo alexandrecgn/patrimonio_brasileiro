3f920152d111378f83b48801763f7a9ce608a181
Alexandre Cavalcanti <alexandrecgn@proton.me>
2025-11-06 21:13:44 -0300

    Adicionar arquivos do NGINX

---
+++ nginx/patrimonio_brasileiro.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# Acidionar caminnho para o Streamlit
+export PATH=/home/alexandrecgn/DevOps/buscador_do_patrimônio/.venv/bin:$PATH
+
+# Mudar para diretório do Patrimônio Brasileiro
+cd /home/alexandrecgn/DevOps/buscador_do_patrimônio
+
+# Ativar Venv
+source .venv/bin/activate
+
+# Iniciar servidor do Streamlit
+streamlit run index.py
