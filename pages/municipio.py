f561a5754ac003e366447bca18862832a10f23bf
Alexandre Cavalcanti <alexandrecgn@proton.me>
2026-04-25 21:25:22 -0300

    Adicionar busca por município

--- pages/municipio.py
+++ pages/municipio.py
@@ -1,5 +1,6 @@
 import json
 import streamlit as st
+from utils import pesquisar_municipio
 from layout import page_config, hero, disclaimer
 
 
@@ -14,11 +15,21 @@ estados = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MG", "MS
 with open("municipios/est_muni.json", "r", encoding="utf8") as file:
     cidades = json.load(file)
 
+st.write("## Busca por município")
+
 uf = st.selectbox(label="Unidade Federativa", options=estados, placeholder="Selecione a UF", index=None)
 
-if uf:
-    municipio = st.selectbox(label="Município", options=cidades[uf], placeholder="Selecione o município", index=None)
+with st.form(key="form", border=False):
+    if uf:
+        municipio = st.selectbox(label="Município", options=cidades[uf], placeholder="Selecione o município", index=None)
 
+        enviado = st.form_submit_button(label="Pesquisar", type="primary")
+
+        if enviado:
+            banco_pontos = "sqlite:////home/alexandrecgn/Developer/patrimonio_brasileiro/bens/bens_pt.sqlite"
+            tabela_pontos = "pontos"
+            resultado = pesquisar_municipio(banco_pontos, tabela_pontos, municipio)
+            resultado
 
 # Carregar o disclaimer.
 disclaimer()
